547babd33c6232f5bdb7b5e59ee3fcf82b0c9695
ZakaKen <komainuzaka@gmail.com>
2021-02-03 00:16:20 +0900

    210202_rust_test

---
+++ rust_test/target/debug/deps/rust_test.d
@@ -0,0 +1,5 @@
+/Users/ken/nand2tetris/rust_test/target/debug/deps/rust_test: src/main.rs
+
+/Users/ken/nand2tetris/rust_test/target/debug/deps/rust_test.d: src/main.rs
+
+src/main.rs:
---
+++ rust_test/target/debug/rust_test.d
@@ -0,0 +1,1 @@
+/Users/ken/nand2tetris/rust_test/target/debug/rust_test: /Users/ken/nand2tetris/rust_test/src/main.rs
