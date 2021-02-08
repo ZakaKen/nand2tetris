4cc95dcd217a57857af492f2e1804c6d9c9ccdf7
ZakaKen <komainuzaka@gmail.com>
2021-02-09 00:06:15 +0900

    210208

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,40 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+	// Put your code here.
+	//init
+	@i
+	M=1
+	@R2
+	M=0
+
+	//LOOP add R0, R1 times
+	(LOOP)
+	//if R1-i<0 then break
+	@i 
+	D=M
+	@R1
+	D=M-D
+	@END
+	D			;JLT
+
+	//add R0 to R2
+	@R0
+	D=M
+	@R2
+	M=M+D
+
+	//increment
+	@i 
+	M=M+1
+
+	//jump to (LOOP)
+	@LOOP
+	0			;JMP
+
+	//END LOOP
+	(END)
+	@END
+	0			;JMP
+
+	
